fb5776e556b372ae337de7c043eb3104aa9656ce
vivalavida20 <vivalavida2051@gmail.com>
2017-03-16 02:06:20 +0900

    postgreSQL로 변경

--- patch_notes/settings.py
+++ patch_notes/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'patch_notes.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'patch_notes',
+        'USER': 'vivalavida20',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
