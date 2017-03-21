28eff6723ee403063ed324c8e746a5762e9c3d16
vivalavida20 <vivalavida2051@gmail.com>
2017-03-22 00:15:55 +0900

    test patch note 추가

--- patch_app/views.py
+++ patch_app/views.py
@@ -4,5 +4,7 @@ from .models import PatchNotes, TestPatchNotes
 
 # Create your views here.
 def patch_notes(request):
-	patch_notes = PatchNotes.objects.filter(title__contains='오버워치 패치 노트') + TestPatchNotes.objects.all()
-	return render(request, 'patch_app/patch_notes.html', {'patch_notes':patch_notes})
+	patch_notes = PatchNotes.objects.filter(title__contains='오버워치 패치 노트')
+	test_patch_notes = TestPatchNotes.objects.all()
+	context = {'patch_notes':patch_notes, 'test_patch_notes':test_patch_notes}
+	return render(request, 'patch_app/patch_notes.html', context=context)
