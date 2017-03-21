49e592f4acb3805e04995df92306e14a29699d35
vivalavida20 <vivalavida2051@gmail.com>
2017-03-22 00:31:00 +0900

    heroku test

--- patch_app/views.py
+++ patch_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .models import PatchNotes, TestPatchNotes
 
 # Create your views here.
+# heroku update test
 def patch_notes(request):
 	patch_notes = PatchNotes.objects.filter(title__contains='오버워치 패치 노트')
 	test_patch_notes = TestPatchNotes.objects.all()
